3eb2ac28ced67461e24c6eff3f7491d7052d2ee4
TBM <32852493+TBM13@users.noreply.github.com>
2019-08-02 15:41:44 -0300

    Super Jump: Initial Commit

---
+++ Super Jump/LegMuscles.cs
@@ -0,0 +1,14 @@
+//File path: Assembly-CSharp.dll/-/HumanHead.cs
+//Only code added/modified by the mod is here.
+
+using System;
+using UnityEngine;
+
+[Serializable]
+public partial class LegMuscles
+{
+  private void JumpAnimation(Vector3 torsoFeedback)
+  {
+    upImpulse = (num4 - momentum.y) * 5f;
+  }
+}
