43dbfec57eea7f14438037da4d0ffde7ecdebab5
TBM <32852493+TBM13@users.noreply.github.com>
2019-08-02 16:18:51 -0300

    Create LegMuscles.cs

---
+++ Super Speed/LegMuscles.cs
@@ -0,0 +1,14 @@
+//File path: Assembly-CSharp.dll/-/LegMuscles.cs
+//Only code added/modified by the mod is here.
+
+using System;
+using UnityEngine;
+
+[Serializable]
+public partial class LegMuscles
+{
+  private void AddWalkForce()
+  {
+    float d = 1300f;
+  }
+}
