0002a3b4ece2c48f124675f03686fc4d5bfa6b48
TBM <mateo.trueba@gmail.com>
2019-11-06 19:32:46 -0300

    Disable ice melt: Add mod

---
+++ Disable ice melt/MeltingObject.cs
@@ -0,0 +1,10 @@
+//File path: Assembly-CSharp.dll/HumanAPI.LightLevel/MeltingObject.cs
+//Only code added/modified by the mod is here.
+
+namespace HumanAPI.LightLevel
+{
+        private void FixedUpdate()
+        {
+            return;
+        }
+}
