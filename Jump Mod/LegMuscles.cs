6823ca5599abb473f897aa571e655dd43b19d5f6
TBM <mateo.trueba@gmail.com>
2019-12-10 14:50:45 -0300

    Jump Mod: Improve jump impulse formula

--- Jump Mod/LegMuscles.cs
+++ Jump Mod/LegMuscles.cs
@@ -9,6 +9,6 @@ public partial class LegMuscles
 {
     private void JumpAnimation(Vector3 torsoFeedback)
     {
-        upImpulse = ((Mathf.Sqrt(4f / Physics.gravity.magnitude) * human.weight) - momentum.y) * CheatCodes.jumpImpulse;
+        upImpulse = ((((Mathf.Sqrt(2f * 0.75f / Physics.gravity.magnitude) + ((Mathf.Pow(Mathf.Clamp(this.human.groundManager.groudSpeed.y, 0f, 100f), 1.2f))) / Physics.gravity.magnitude)) * this.human.weight) - momentum.y) * CheatCodes.jumpImpulse;
     }
 }
